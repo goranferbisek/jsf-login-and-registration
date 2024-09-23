7067277131733b7d3242fae3fd91f1c5c76486b3
Goran Ferbišek <goran.ferbisek@gmail.com>
2024-09-23 22:34:24 +0200

    Set an identity store

--- src/main/java/si/ferbisek/jsfloginandregistration/ApplicationConfig.java
+++ src/main/java/si/ferbisek/jsfloginandregistration/ApplicationConfig.java
@@ -1,9 +1,11 @@
 package si.ferbisek.jsfloginandregistration;
 
+import jakarta.annotation.sql.DataSourceDefinition;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.faces.annotation.FacesConfig;
 import jakarta.security.enterprise.authentication.mechanism.http.CustomFormAuthenticationMechanismDefinition;
 import jakarta.security.enterprise.authentication.mechanism.http.LoginToContinue;
+import jakarta.security.enterprise.identitystore.DatabaseIdentityStoreDefinition;
 
 @CustomFormAuthenticationMechanismDefinition(
         loginToContinue = @LoginToContinue(
@@ -11,6 +13,16 @@ import jakarta.security.enterprise.authentication.mechanism.http.LoginToContinue
                 useForwardToLogin = false,
                 errorPage = ""
         )
+)
+@DatabaseIdentityStoreDefinition(
+        dataSourceLookup = "java:app/MyDataSource",
+        callerQuery = "SELECT password FROM users WHERE username = ?",
+        groupsQuery = "SELECT group_name FROM user_groups WHERE username = ?"
+)
+@DataSourceDefinition(
+        name = "java:app/MyDataSource",
+        className = "org.h2.jdbcx.JdbcDataSource",
+        url = "jdbc:h2:~/test:MODE=LEGACY;DB_CLOSE_ON_EXIT=FALSE"
 )
 @FacesConfig
 @ApplicationScoped
